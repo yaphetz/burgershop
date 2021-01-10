7c5f2536cfd40c125ad6ab6e9c2d65e019d7c46e
Marius <Marius@DESKTOP-RPDOLTQ>
2021-01-10 23:24:22 +0200

    more than 1 product in cart

--- cart.php
+++ cart.php
@@ -17,7 +17,8 @@
 <?php
  
  $db = mysqli_connect('localhost', 'bazaproduse', 'Wasd123!@#', 'bazaproduse');
- $sql = "SELECT shoppingcart.ShoppingCartId as `ShoppingCartID`, shoppingcart.ClientId as `ShoppingCartClientId`, shoppingcart.TotalAmount as CartTotalPrice, shoppingcart.HasExpired as ShoppingCartExpiryDate, shoppingcartproduct.ProductId, product.ProductId as ProductID, product.Name as ProductName, product.Price as ProductPrice, product.Picture as ProductPicture, product.ExpirationDate as ProductExpiryDate FROM `shoppingcartproduct` JOIN product on ShoppingCartProduct.ProductId = product.ProductId inner join shoppingcart on shoppingcartproduct.ShoppingCartId = shoppingcart.ShoppingCartId WHERE shoppingcart.ClientId = $ClientId ";
+ $sql = "SELECT shoppingcart.ShoppingCartId as `ShoppingCartID`, shoppingcart.ClientId as `ShoppingCartClientId`, shoppingcart.TotalAmount as CartTotalPrice, shoppingcart.HasExpired as ShoppingCartExpiryDate, shoppingcartproduct.ProductId, GROUP_CONCAT(product.ProductId SEPARATOR ',') as ProductID, GROUP_CONCAT(product.Name SEPARATOR ',') as ProductName, GROUP_CONCAT(product.Price SEPARATOR ',') as ProductPrice, GROUP_CONCAT(product.Picture SEPARATOR ',') as ProductPicture, GROUP_CONCAT(product.ExpirationDate SEPARATOR ',') as ProductExpiryDate FROM `shoppingcartproduct` JOIN product on ShoppingCartProduct.ProductId = product.ProductId inner join shoppingcart on shoppingcartproduct.ShoppingCartId = shoppingcart.ShoppingCartId WHERE shoppingcart.ClientId = $ClientId
+ GROUP by shoppingcart.ShoppingCartId";
  $result = mysqli_query($db, $sql);
  foreach ($result as $row) {
    printf('
@@ -28,19 +29,31 @@
 <li class="list-group-item">Expiration date: %s</li>
 </ul>
 
-<div id="modalcart" class="modal container" >
-<ul class="list-group">
-<li class="list-group-item">Product Picture: <img height="150px" src="%s"></li>
-<li class="list-group-item">Price: %s</li>
-<li class="list-group-item">Expiration date: %s</li>
-</ul>
-</div>
-
-<a href="#modalcart" rel="modal:open">  <button type="button" class="btn btn-info" >Open Cart</button> </a>
+<a href="#modalcartwithproducts" rel="modal:open">  <button type="button" class="btn btn-info" >Open Cart</button> </a>
 </div>
 <br>
-',$row['ShoppingCartID'], $row['ShoppingCartID'], $row['CartTotalPrice'], $row['ShoppingCartExpiryDate'], $row['ProductPicture'], $row['ProductPrice'], $row['ProductExpiryDate']);
+',$row['ShoppingCartID'], $row['ShoppingCartID'], $row['CartTotalPrice'], $row['ShoppingCartExpiryDate']);
  }
+
+ foreach ($result as $row) {
+	$index = 0;
+	echo ('<div id="modalcartwithproducts" class="modal container">');
+	for($i=0;$i<3;$i++)
+	{
+	printf('
+	<ul class="list-group">
+	<li class="list-group-item">Product Picture: <img height="75px" src="%s"></li>
+	<li class="list-group-item">Price: %s</li>
+	<li class="list-group-item">Expiration date: %s</li>
+	</ul> <br>'
+	,(explode(",",$row['ProductPicture']))[$i], (explode(",",$row['ProductPrice']))[$i], (explode(",",$row['ProductExpiryDate'])[$i]));
+	}
+	echo '</div>';
+	$index++;
+	//$row['ProductPicture'], $row['ProductPrice'], $row['ProductExpiryDate']);
+ }
+
+
  mysqli_close($db);
 
 ?>
